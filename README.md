55e44bcb237fd6d0610f6d0f6b28279e08b7a558
Violet <78319593+fadicek@users.noreply.github.com>
2022-02-10 23:12:07 +0500

    cs
    cs

---
+++ README.md
@@ -0,0 +1,2 @@
+# Developing-Website
+ Violet Website
---
+++ site/js/title.js
@@ -0,0 +1,3 @@
+if(document.addEventListener){document.addEventListener("DOMContentLoaded",function(){loaded();});}else if(document.attachEvent){document.attachEvent("onreadystatechange",function(){loaded();});}
+function loaded(){setInterval(loop,250);}
+var x=0;var titleText=["🖤", "n","ne","nev","neve","never","neverw","neverwi","neverwin","neverwin","neverwin","neverwin","neverwin","🖤"];function loop(){document.getElementsByTagName("title")[0].innerHTML=titleText[x++%titleText.length];}
